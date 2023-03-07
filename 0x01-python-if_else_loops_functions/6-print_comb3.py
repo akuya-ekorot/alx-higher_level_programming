c84dd8d2ec819e56f0146074e6478dd62c790014
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-07 12:21:38 +0300

    format specifiers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(9):
     for j in range(10):
         if j > i:
             if i != 8:
-                print(f"{i}{j}", end=", ")
+                print("{0}{1}".format(i, j), end=", ")
             else:
-                print(f"{i}{j}")
+                print("{0}{1}".format(i, j))
