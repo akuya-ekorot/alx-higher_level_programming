532648e845cab1bb6a07ae87045d71825ecc4eca
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-09 19:22:22 +0300

    calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import calculator_1 as calc
 
-a = 10
-b = 5
-
 if __name__ == "__main__":
+    a = 10
+    b = 5
+
     print("{0} + {1} = {2}".format(a, b, calc.add(a, b)))
     print("{0} - {1} = {2}".format(a, b, calc.sub(a, b)))
     print("{0} * {1} = {2}".format(a, b, calc.mul(a, b)))
