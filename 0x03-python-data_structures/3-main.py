26155716a6ecf0cd7f95ffe6a37b31f08e7c217c
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-13 12:05:29 +0300

    delete at

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -2,4 +2,13 @@
 print_reversed_list_integer = __import__("3-print_reversed_list_integer")\
                                 .print_reversed_list_integer
 my_list = [1, 2, 3, 4, 5]
+print("Working with list: {}".format(my_list))
+print_reversed_list_integer(my_list)
+
+my_list = []
+print("Working with list: {}".format(my_list))
+print_reversed_list_integer(my_list)
+
+my_list = [3]
+print("Working with list: {}".format(my_list))
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -4,3 +4,11 @@ max_integer = __import__("9-max_integer").max_integer
 my_list = [1, 90, 2, 13, 34, 5, -13, 3]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
+
+my_list = []
+max_value = max_integer(my_list)
+
+if max_value is None:
+    print("Passed: Expected None and got {}".format(max_value))
+else:
+    print("Failed: Expected None but got {}".format(max_value))
