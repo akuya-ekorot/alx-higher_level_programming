11a4338dd8577b95d22ee17651db0d1924e4d6b4
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-13 11:22:37 +0300

    replace in list

--- 0x03-python-data_structures/2-main.py
+++ 0x03-python-data_structures/2-main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 replace_in_list = __import__("2-replace_in_list").replace_in_list
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
+og_list = [1, 2, 3, 4, 5]
+print("Working with list: {}".format(og_list))
 
-print(new_list)
-print(my_list)
+idx = 4
+el = 9
+new_list = replace_in_list(og_list, idx, el)
+
+print("Working with list: {}".format(og_list))
+print("New list: {}".format(new_list))
