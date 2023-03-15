e6dd7afafda95b9f5d6703af3ea0d018b8980f1f
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-15 10:58:19 +0300

    only diff elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,8 +3,7 @@ def common_elements(set_1, set_2):
     common_list = []
 
     for i in sorted(list(set_1)):
-        for j in sorted(list(set_2)):
-            if i is j:
-                common_list.append(i)
+        if i in sorted(list(set_2)):
+            common_list.append(i)
 
     return set(common_list)
