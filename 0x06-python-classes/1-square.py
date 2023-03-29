d40889f5949861af06f90199beee1efe076c4e06
Akuya Ekorot <akuyaekorot@gmail.com>
2023-03-29 05:53:48 +0300

    private attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains the class Square
+"""
+
+
+class Square:
+    """This is a square class
+    """
+
+    def __init__(self, size):
+        """initializer function"""
+        self.__size = size
