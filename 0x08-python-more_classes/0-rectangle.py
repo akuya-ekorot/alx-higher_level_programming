7e57fa4ff54b52b79b1e103b7ce2618492239f11
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-05 14:28:14 +0300

    task: write an empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines the class Rectangle"""
+
+
+class Rectangle:
+    """An empty class"""
+
+    def __init__(self):
+        self.__dict__ = {}
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# Python More Classes
+
+This directory contains tasks for the project Python More Classes from ALX SE.
