d496a1418c27f5c45f8fab0d96cfabdab21cb1eb
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-06 00:23:31 +0300

    task: implement doctest for script

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module hosts the function add_integer"""
+
+
+def add_integer(a, b=98):
+    """Returns the sum of a and b."""
+
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
