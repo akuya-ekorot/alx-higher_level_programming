b4a40bc3f153b94c8109b975667f59e60d7bc8a8
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-06 23:04:52 +0300

    task: text indentation and test

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pyhton3
+"""This module contains the function text_indentation"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of the characters '.', '?', ':'
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    print("{}".format(text)
+          .replace(". ", "\n\n")
+          .replace("? ", "\n\n")
+          .replace(": ", "\n\n"))
