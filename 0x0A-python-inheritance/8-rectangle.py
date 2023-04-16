f4c6b6fcc39f09a8be522d1d5f71e83e13f99e3f
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-16 20:48:38 +0300

    fix: inherit basegeometry from task 7

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
-"""contains class BaseGeometry and rectangle"""
-
-
-class BaseGeometry:
-    """Empty class"""
-
-    def area(self):
-        """method not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value"""
-        if (type(value) != int):
-            raise TypeError("{} must be an integer".format(name))
-        if (value <= 0):
-            raise ValueError("{} must be greater than 0".format(name))
+"""contains class Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
