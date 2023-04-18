5939c87d7222a18e0fe408989d04f1621acee149
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-18 22:43:15 +0300

    task: validate attributes

--- 0x0C-python-almost_a_circle/1-main.py
+++ 0x0C-python-almost_a_circle/1-main.py
@@ -5,9 +5,15 @@ from models.rectangle import Rectangle
 if __name__ == "__main__":
     r1 = Rectangle(10, 2)
     print(r1.id)
+    print(r1.width)
+    print(r1.height)
 
     r2 = Rectangle(2, 10)
     print(r2.id)
+    print(r2.width)
+    print(r2.height)
 
     r3 = Rectangle(10, 2, 0, 0, 12)
     print(r3.id)
+    print(r3.width)
+    print(r3.height)
