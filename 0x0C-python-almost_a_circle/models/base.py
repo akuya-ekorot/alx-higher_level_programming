f2396afa15559b680170a8661590ab4fda976448
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-19 02:02:15 +0300

    task: json string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,19 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+
+        dictionaries = []
+
+        for obj in list_objs:
+            dictionaries.append(obj.to_dictionary())
+
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(dictionaries, f)
+
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns json representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
