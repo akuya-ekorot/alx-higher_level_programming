7130cd45459d9c3f25820e19b10e79453d1ef617
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-19 00:34:32 +0300

    task: method update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,20 @@ class Square(Rectangle):
                                              self.y,
                                              self.size)
 
+    def update(self, *args, **kwargs):
+        """Updates the values of the instance
+        """
+        attributes = ["id", "size", "x", "y"]
+
+        if args:
+
+            for i, arg in enumerate(args):
+                setattr(self, attributes[i], arg)
+        else:
+            for key, value in kwargs.items():
+                if key in attributes:
+                    setattr(self, key, value)
+
     @property
     def size(self):
         """Getter for the private property size
