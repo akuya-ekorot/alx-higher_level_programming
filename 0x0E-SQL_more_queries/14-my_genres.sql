4a25cf71395c33f4150f7e7b0db497c61812d6ab
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-20 13:48:28 +0300

    double join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -7,4 +7,5 @@
 SELECT tv_genres.name AS name
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Dexter';
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.name = 'Dexter';
