26664526b707f6cdcef7041ba9844b4880b34ca5
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-20 10:27:34 +0300

    remove grant option when granting privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 -- if the user already exists, the script should not fail
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
