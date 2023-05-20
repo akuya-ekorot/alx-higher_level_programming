ae4addb0c02a5cc256e526eeb0a3337dd7d65e18
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-20 13:49:39 +0300

    fix field name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -8,4 +8,4 @@ SELECT tv_genres.name AS name
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.name = 'Dexter';
+WHERE tv_shows.title = 'Dexter';
