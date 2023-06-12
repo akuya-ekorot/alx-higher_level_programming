3490f49106aa1de953ed071d61e56ac0ea36bf6c
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-12 16:01:08 +0300

    parseInt

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ try {
   const number = parseInt(process.argv[2]);
 
   if (number) {
-    console.log('My number:', number);
+    console.log(`My number: ${number}`);
   } else {
     throw new Error();
   }
