4a51f1d2887d32d6c1af52db92af96292831e829
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-12 16:19:35 +0300

    i love c: fix negative value

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ let iter = process.argv[2];
 if (!iter) {
   console.log('Missing number of occurrences');
 } else {
-  while (iter--) {
+  while (iter-- && iter > 0) {
     console.log('C is fun');
   }
 }
