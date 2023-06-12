70bd746ae584c56ef1edfacf2880c2071fdda344
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-12 15:57:52 +0300

    parseInt

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+try {
+  const number = parseInt(process.argv[2]);
+
+  if (number) {
+    console.log('My number:', number);
+  } else {
+    throw new Error;
+  }
+
+} catch (e) {
+  console.log('Not a number');
+}
