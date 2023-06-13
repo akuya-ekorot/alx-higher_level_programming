575998776780b21203a4ddfb349c6270d491f194
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-13 21:08:09 +0300

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,6 +6,6 @@ const [Path1, Path2, dest] = process.argv.splice(2);
 const file1 = fs.readFileSync(Path1, 'utf8');
 const file2 = fs.readFileSync(Path2, 'utf8');
 
-const concated = file1 + '\n' + file2;
+const concated = file1 + file2;
 
 fs.writeFileSync(dest, concated, 'utf8');
