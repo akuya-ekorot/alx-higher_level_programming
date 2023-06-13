77f3e6317775631a026f7049aa0a28af93045a91
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-13 13:44:33 +0300

    remove constructor

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (height, width) {
-    this.height = height;
-    this.width = width;
-  }
 }
 
 module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/tests/0-rectangle.test.js
+++ 0x13-javascript_objects_scopes_closures/tests/0-rectangle.test.js
@@ -4,9 +4,11 @@ const Rectangle = require('../0-rectangle');
 const r1 = new Rectangle();
 
 test('check instance of 0-rectangle', () => {
+  console.log(r1);
   expect(r1).toBeInstanceOf(Rectangle);
 });
 
 test('check instance of constructor', () => {
+  console.log(r1.constructor);
   expect(r1.constructor).toBeInstanceOf(Rectangle.constructor)
 })
