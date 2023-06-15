1debc1feed2ac847c5aac3136d8eb1e2a1169c02
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-15 12:10:30 +0300

    first query with sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Fetch all from tables in database
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # initialize engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # build query and execute
+    query = session.query(State)
+    query = query.order_by(State.id)
+    results = query.all()
+
+    # print results
+    for result in results:
+        print("{}: {}".format(result.id, result.name))
+
+    # close session
+    session.close()
