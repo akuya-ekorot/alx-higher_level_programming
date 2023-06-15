13f40fd60010cb2ffa386a89e0efaa9f5a9da9a6
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-15 09:27:39 +0300

    set up mysqldb and retrieve data

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import MySQLdb
+
+# get command line arguments
+username, password, db_name = sys.argv[1:]
+
+# create db
+db = MySQLdb.connect(
+        host='localhost',
+        user=username,
+        passwd=password,
+        db=db_name)
+
+# create cursor
+cur = db.cursor()
+
+# execute command
+cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+states = cur.fetchall()
+
+# print results
+for state in states:
+    print(state)
