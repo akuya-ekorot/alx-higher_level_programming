393e31ae27ec4de077da92c7c57c0f1d1c869ca7
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-16 11:02:37 +0300

    add docs to model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
+"""Module to create a class State and an instance Base"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Instance of declarative_base
 Base = declarative_base()
 
 
 class State(Base):
+    """Class State that inherits from Base
+    Args:
+        Base: Instance of declarative_base
+    Attributes:
+        id: Integer column id that is primary key
+        name: String column name
+    """
+
     __tablename__ = 'states'
     id = Column(
             Integer,
