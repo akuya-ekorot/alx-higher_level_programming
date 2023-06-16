b27f2e0fb0336eb7c7ed8f527dbfac17319f7e83
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-16 12:06:14 +0300

    refactor model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""Create a class definition of a City
+"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
 class City(Base):
+    """Class definition of a City
+    Args:
+        Base (class): base class from which to inherit
+
+    Attributes:
+        id (int): unique identifier
+        name (str): name of city
+        state_id (int): id of state
+    """
     __tablename__ = 'cities'
     id = Column(
             Integer,
