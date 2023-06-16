e84784ad5458c9cb9da96729ba7ea4db048350cf
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-16 10:58:52 +0300

    refactor filter cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,39 +1,53 @@
 #!/usr/bin/python3
+"""Module to list all cities from the database hbtn_0e_4_usa
+"""
 
 import sys
 import MySQLdb
 
-# get command line arguments
-username, password, db_name, filter = sys.argv[1:]
 
-# create db
-db = MySQLdb.connect(
-        host='localhost',
-        user=username,
-        passwd=password,
-        db=db_name)
+# function to get all cities by state
+def get_cities_by_state(db):
+    """Function to list all cities from the database hbtn_0e_4_usa
+    """
+    # create cursor
+    cur = db.cursor()
 
-# create cursor
-cur = db.cursor()
+    # craft query
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states
+    ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC;
+    """
 
-# craft query
-query = """
-SELECT cities.name
-FROM cities
-JOIN states
-ON cities.state_id = states.id
-WHERE states.name = %s
-ORDER BY cities.id ASC;
-"""
+    # execute and fetch query
+    cur.execute(query, (filter,))
+    results = cur.fetchall()
 
-# execute and fetch query
-cur.execute(query, (filter,))
-results = cur.fetchall()
+    # print results
+    city_names = [city[0] for city in results]
+    print(', '.join(city_names))
 
-# print results
-city_names = [city[0] for city in results]
-print(', '.join(city_names))
+    # close the cursor and db
+    cur.close()
 
-# close the cursor and db
-cur.close()
-db.close()
+
+if __name__ == "__main__":
+    # get command line arguments
+    username, password, db_name, filter = sys.argv[1:5]
+
+    # create db
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=db_name)
+
+    # get all cities by state
+    get_cities_by_state(db)
+
+    # close db
+    db.close()
