fb5628d2ca7a446963277501e467da6237b4ca20
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-16 08:35:38 +0300

    filter states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,46 @@
 #!/usr/bin/env python3
+"""script that lists all states with a name starting with N (upper N)"""
 
 import sys
 import MySQLdb
 
-# get command line arguments
-username, password, db_name = sys.argv[1:]
 
-# create db
-db = MySQLdb.connect(
+# function to filter states
+def filter_states(db):
+    """filters states that start with 'N'"""
+
+    # create cursor
+    cur = db.cursor()
+    query = """
+    SELECT id, name
+    FROM states
+    WHERE name LIKE 'N%'
+    ORDER BY id ASC;
+    """
+
+    # execute command
+    cur.execute(query)
+    filtered_states = cur.fetchall()
+
+    # print results
+    for state in filtered_states:
+        print(state)
+
+
+if __name__ == "__main__":
+    # get command line arguments
+    username, password, db_name = sys.argv[1:]
+
+    # create db
+    db = MySQLdb.connect(
         host='localhost',
         user=username,
         passwd=password,
-        db=db_name)
+        db=db_name
+    )
 
-# create cursor
-cur = db.cursor()
-query = """
-SELECT id, name
-FROM states
-WHERE name LIKE 'N%'
-ORDER BY id ASC;
-"""
+    # filter states
+    filter_states(db)
 
-# execute command
-cur.execute(query)
-filtered_states = cur.fetchall()
-
-# print results
-for state in filtered_states:
-    print(state)
+    # close cursor and database
+    db.close()
