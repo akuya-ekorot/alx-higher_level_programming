16c075470974f9ecc75fab7ef5a7074b17dc7413
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-16 08:10:28 +0300

    list all state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Script that lists all states from the database hbtn_0e_0_usa """
 
 import sys
 import MySQLdb
 
-# get command line arguments
-username, password, db_name = sys.argv[1:]
 
-# create db
-db = MySQLdb.connect(
-        host='localhost',
-        user=username,
-        passwd=password,
-        db=db_name)
+# function to list all states from database
+def list_all_states():
+    """ Lists all states from database """
 
-# create cursor
-cur = db.cursor()
+    # get command line arguments
+    username, password, db_name = sys.argv[1:]
 
-# execute command
-cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
-states = cur.fetchall()
+    # create db
+    db = MySQLdb.connect(
+            host='localhost',
+            user=username,
+            passwd=password,
+            db=db_name)
 
-# print results
-for state in states:
-    print(state)
+    # create cursor
+    cur = db.cursor()
+
+    # execute command
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC;")
+    states = cur.fetchall()
+
+    # print results
+    for state in states:
+        print(state)
+
+
+if __name__ == '__main__':
+    list_all_states()
