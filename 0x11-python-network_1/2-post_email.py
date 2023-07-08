518bccd477f617b7cfa47c142633f24be54aa7fd
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-08 18:20:32 +0300

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Takes in a url and an email address, sends a POST request to the passed url
+with the email as a parameter, and displays the body of the response (decoded
+in utf-8)"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
+
+    with urllib.request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
