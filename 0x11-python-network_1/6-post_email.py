f6cf84a64958942459e2c55d18c3fb8c35eacedd
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-08 22:52:11 +0300

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Python script that takes in a URL and an email address, sends a POST request
 to the passed URL with the email as a parameter, and finally displays the body
 of the response."""
