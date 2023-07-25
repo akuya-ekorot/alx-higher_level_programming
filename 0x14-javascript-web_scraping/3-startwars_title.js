4ad5bd2695a7abb1f7fdb5bc49515128f02712a7
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-25 12:32:57 +0300

    star wars movie title

---
+++ 0x14-javascript-web_scraping/3-startwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const json = JSON.parse(body);
+    console.log(json.title);
+  }
+});
