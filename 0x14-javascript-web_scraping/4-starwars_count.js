62537d483aecc7dab4bbc093244b7c78a0005834
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-25 12:49:40 +0300

    star wars wedge

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+const wedge = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+
+    let count = 0;
+
+    for (let i = 0; i < films.length; i++) {
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === wedge) {
+          count++;
+        }
+      }
+    }
+
+    console.log(count);
+  }
+});
