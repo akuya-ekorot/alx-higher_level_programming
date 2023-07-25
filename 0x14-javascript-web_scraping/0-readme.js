0729c7651f61da73bcb6c4fa5d0bc27826e04498
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-25 12:14:38 +0300

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@
 // The content of the file must be read in utf-8.
 // If an error occurred during the reading, print the error object.
 
-const fs = require("fs");
+const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, "utf-8", (err, data) => {
+fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
