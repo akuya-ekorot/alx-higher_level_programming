d6442de69789c8ce08a9807bed1dc82fb77537b3
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-25 12:11:39 +0300

    readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// This script reads and prints the content of a file.
+// The first argument is the file path.
+// The content of the file must be read in utf-8.
+// If an error occurred during the reading, print the error object.
+
+const fs = require("fs");
+const file = process.argv[2];
+
+fs.readFile(file, "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
